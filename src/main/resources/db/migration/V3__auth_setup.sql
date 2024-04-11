f2b551adf99c6e44881e2944e8c02a8399624603
tmiksik <t.miksik@reply.de>
2024-04-11 18:03:17 +0200

    Untrack files in .gitignore

--- src/main/resources/db/migration/V3__auth_setup.sql
+++
@@ -1,7 +0,0 @@
-create table authorities
-(
-    username  varchar(255) not null unique,
-    authority varchar(50)  not null,
-    constraint fk_authorities_users foreign key (username) references users (username)
-);
-create unique index ix_auth_username on authorities (username, authority);
